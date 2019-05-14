8626d068c4c2528e381e2609c0738977093e5c6b
MustafaElsanadidy <49868815+MustafaElsanadidy@users.noreply.github.com>
2019-05-14 03:43:12 -0700

    Initial Commit

---
+++ MainView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MainView
+//
+//  Created by 68lion on 5/14/19.
+//  Copyright © 2019 68lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
